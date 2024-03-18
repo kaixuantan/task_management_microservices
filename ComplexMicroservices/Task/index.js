34e02afb26673a639740f26d1443e0fb4f716841
netteee <lynettetay48@gmail.com>
2024-03-18 13:23:44 +0800

    task complex microservice

--- ComplexMicroservices/Task/index.js
+++ ComplexMicroservices/Task/index.js
@@ -1,38 +1,102 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
+const PORT = process.env.PORT;
+const SubGrpAppId = process.env.SubGrpAppId;
+const SubGrpKey = process.env.SubGrpKey;
+const TaskAppId = process.env.TaskAppId;
+const TaskKey = process.env.TaskKey;
+const LogAppId = process.env.LogAppId;
+const LogKey = process.env.LogKey;
 
-app.get('/users', async (req, res) => {
+app.get('/subgroup/:subGroupId', async (req, res) => {
     try {
-        const headers = {
-            'X-User-AppId': 'ifZ1JapLTp14J63bxoIr',
-            'X-User-Key': '40KLo5jw8LSUOaeqHAY3EJC8REWEU1',
-        };
+        const subGroupId = req.params.subGroupId;
     
-        // fetch all users 
-        const usersResponse = await axios.get('https://personal-rc7vnnm9.outsystemscloud.com/UserAPI_REST/rest/v1/user', {
-            headers, 
+        const subGrpResponse = await axios.get(`https://personal-rc7vnnm9.outsystemscloud.com/SubGroupAPI_REST/rest/v1/subgroup/${subGroupId}`, {
+            headers: {
+                'X-SubGroup-AppId': SubGrpAppId,
+                'X-SubGroup-Key': SubGrpKey,
+            }
         });
 
-        const users = {};
-        usersResponse.data.User.forEach(user => {
-            users[user.userId] = user.username;
+        const assignees = {};
+        subGrpResponse.data.SubGroup.subGroupUsers.forEach(assignee => {
+            assignees[assignee.userId] = assignee.username;
         });
 
-        res.json({ users });
+        res.json(assignees);
     } catch (error) {
-        console.error('Error creating task:', error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
+        // console.log(error.message)
+        sendLog(req.body.userId, 'Error retrieving assignees: ' + error.message);
+
+        if (error.code === 'ERR_BAD_RESPONSE') {
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(error.response.status).json({ 
+                code: error.response.status + " " + error.response.statusText,
+                error: error.response.data.Result.ErrorMessage
+            });
+        }
     }
 });
-  
-app.get('/', (req, res) => {
-    res.send('Welcome to Task Creation Complex Microservice');
+
+app.post('/task', async (req, res) => {
+    try {
+        await axios.post(
+            `https://personal-rc7vnnm9.outsystemscloud.com/SubGroupAPI_REST/rest/v1/task`,
+            {
+                name: req.body.taskName,
+                subGroupId: req.body.subGroupId,
+                createdById: req.body.userId,
+                assigneeId: req.body.assigneeId
+            }, 
+            {
+                headers: {
+                    'X-Task-AppId': TaskAppId,
+                    'X-Task-Key': TaskKey,
+                }
+            }
+        );
+
+        res.status(201).json({ message: 'Task created successfully' });
+    } catch (error) {
+        sendLog(req.body.userId, 'Error creating task: ' + error.message);
+
+        if (error.code === 'ERR_BAD_RESPONSE') {
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.status(error.response.status).json({ 
+                code: error.response.status + " " + error.response.statusText,
+                error: error.response.data.Result.ErrorMessage
+            });
+        }
+    }
 });
 
+async function sendLog(userId, message) {
+    try {
+        await axios.post(`https://personal-rc7vnnm9.outsystemscloud.com/LogAPI_REST/rest/v1/log`, 
+        {
+            userId: userId,
+            type: "task",
+            description: message
+        },
+        {
+            headers: {
+                'X-Log-AppId': LogAppId,
+                'X-Log-Key': LogKey,
+            },
+        });
+    } catch (error) {
+        console.error('Error sending log to logging microservice:', error.message);
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
