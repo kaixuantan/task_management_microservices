0baeb3623e2593953346ca2864d000bd36d61e32
Kelyn Wong <kelynwonget@gmail.com>
2024-03-19 13:53:18 +0800

    change connection to retry if fail to connect

--- SimpleMicroservices/Notification/notificationServer.py
+++ SimpleMicroservices/Notification/notificationServer.py
@@ -1,3 +1,4 @@
+import time
 import pika
 import smtplib
 import json
@@ -18,9 +19,40 @@ smtp_username = os.getenv('SMTP_USERNAME')
 smtp_password = os.getenv('SMTP_PASSWORD')
 
 # Connect to RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters
-                                     (host=rabbitmq_host, port=rabbitmq_port,
-                                      heartbeat=3600, blocked_connection_timeout=3600))
+def create_connection(max_retries=12, retry_interval=5):
+    print('notificationServer:create_connection')
+    
+    retries = 0
+    connection = None
+    
+    # loop to retry connection upto 12 times with a retry interval of 5 seconds
+    while retries < max_retries:
+        try:
+            print('notificationServer: Trying connection')
+            # connect to the broker and set up a communication channel in the connection
+            connection = pika.BlockingConnection(pika.ConnectionParameters
+                                (host=rabbitmq_host, port=rabbitmq_port,
+                                 heartbeat=3600, blocked_connection_timeout=3600)) # these parameters to prolong the expiration time (in seconds) of the connection
+                # Note about AMQP connection: various network firewalls, filters, gateways (e.g., SMU VPN on wifi), may hinder the connections;
+                # If "pika.exceptions.AMQPConnectionError" happens, may try again after disconnecting the wifi and/or disabling firewalls.
+                # If see: Stream connection lost: ConnectionResetError(10054, 'An existing connection was forcibly closed by the remote host', None, 10054, None)
+                # - Try: simply re-run the program or refresh the page.
+                # For rare cases, it's incompatibility between RabbitMQ and the machine running it,
+                # - Use the Docker version of RabbitMQ instead: https://www.rabbitmq.com/download.html
+            print("notificationServer: Connection established successfully")
+            break  # Connection successful, exit the loop
+        except pika.exceptions.AMQPConnectionError as e:
+            print(f"notificationServer: Failed to connect: {e}")
+            retries += 1
+            print(f"notificationServer: Retrying in {retry_interval} seconds...")
+            time.sleep(retry_interval)
+
+    if connection is None:
+        raise Exception("amqp_setup: Unable to establish a connection to RabbitMQ after multiple attempts.")
+
+    return connection
+
+connection = create_connection()
 channel = connection.channel()
 
 # Declare the queue
