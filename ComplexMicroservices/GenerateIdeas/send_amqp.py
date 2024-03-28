8c926238724eb2c39ea60ca7ea0d5bc47a398b1a
Kai Xuan <tankaixuan24@gmail.com>
2024-03-28 15:12:26 +0800

    docker stuff

--- ComplexMicroservices/GenerateIdeas/send_amqp.py
+++ ComplexMicroservices/GenerateIdeas/send_amqp.py
@@ -17,6 +17,8 @@ smtp_port = os.getenv('SMTP_PORT')
 smtp_username = os.getenv('SMTP_USERNAME')
 smtp_password = os.getenv('SMTP_PASSWORD')
 
+print(f"RabbitMQ Host: {rabbitmq_host}")
+
 # Connect to RabbitMQ
 connection = pika.BlockingConnection(pika.ConnectionParameters
                                      (host=rabbitmq_host, port=rabbitmq_port,
