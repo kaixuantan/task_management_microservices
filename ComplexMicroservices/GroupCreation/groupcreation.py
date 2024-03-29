909ba50a8403da17b58106989f3547026b4f069b
Kai Xuan <tankaixuan24@gmail.com>
2024-03-29 10:29:24 +0800

    added wait for it for complex microservices, and edited docker compose

--- ComplexMicroservices/GroupCreation/groupcreation.py
+++ ComplexMicroservices/GroupCreation/groupcreation.py
@@ -310,4 +310,4 @@ def processUserAssignment(group,user_id_list,subgroup_namelist):
 if __name__ == "__main__":
     print("This is flask " + os.path.basename(__file__) +
           " for creating a group...")
-    app.run(host="0.0.0.0", port=5100, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- ComplexMicroservices/enrollment/enrollment.py
+++ ComplexMicroservices/enrollment/enrollment.py
@@ -199,4 +199,4 @@ def notify_admin():
         return jsonify({'message': 'User enrollment failed'}), 400
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5009, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
